08ac3c004d5d0fdc8846ce3ef6c4ad49e1ea8167
Darvesh Gorhe <dgorhe123@gmail.com>
2021-12-30 10:25:18 -0800

    recipe scraping

---
+++ firebase/functions/call.py
@@ -0,0 +1,22 @@
+import requests
+
+# Check number of arguments
+
+# Print help
+
+# Format request
+url = ""
+
+headers = {
+
+}
+
+body = {
+
+}
+
+# Send request
+print("Calling url:", url)
+
+
+# Print results of request
