e09951ffcfff78c2bbe9a71dfa1e896947b99199
Darvesh Gorhe <dgorhe123@gmail.com>
2021-12-31 13:10:51 -0800

    added functions to insert and remove ingredients, recipes, and users

--- firebase/functions/src/operations.ts
+++ firebase/functions/src/operations.ts
@@ -1,9 +1,42 @@
-/*
- *
- */
+// import { Recipe, User, Ingredient } from "./lib";
 
-export const _addIngredientToPantry = async (ingredients: string[], auth: any) => {
-    console.log("_addIngredientToPantry");
+/*
+ * Ingredient Operations
+ */
+export const _addIngredient = async (ingredients: string | string[], auth: any) => {
+    if (Array.isArray(ingredients)) {
+        // add ingredients in batch, remember to use try-catch
+    } else {
+        // add single ingredient, remember to use try-catch
+    }
     console.log(ingredients);
     console.log(auth);
 };
+
+export const _removeIngredient = async (ingredientId: string, auth: any) => {
+
+};
+
+/*
+ * Recipe Operations
+ */
+export const _addRecipe = async (data: {string: string[]}, auth: any) => {
+
+};
+
+export const _removeRecipe = async (recipeId: string | string[], auth: any) => {
+
+};
+
+
+/*
+ * User Operations
+ */
+export const _addUser = async (data: {string: string[]}, auth: any) => {
+
+};
+
+export const _removeUser = async (userId: string | string[], auth: any) => {
+
+};
+
