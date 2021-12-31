d2a87fc7b9a3b525fcdc1ae45ee1aee6aa4996bd
Darvesh Gorhe <dgorhe123@gmail.com>
2021-12-31 11:00:35 -0800

    basic argument parsing to speed up Cloud Function testing

--- firebase/functions/call.py
+++ firebase/functions/call.py
@@ -1,22 +1,58 @@
+import argparse
 import requests
 
-# Check number of arguments
+parser = argparse.ArgumentParser(description = "Call Firebase Functions via command line")
 
-# Print help
+# Positional arg for function to call
+parser.add_argument(
+        'function', 
+        nargs=1,
+        type=str, 
+        help='function name to call',
+)
 
-# Format request
-url = ""
+# Whether the call is for emulator or deployment
+parser.add_argument(
+        '--local', 
+        nargs=1, 
+        type=bool, 
+        choices=[True, False], 
+        default=True
+)
 
-headers = {
+# JSON body to send to function
+parser.add_argument(
+        '--body',
+        nargs=1,
+        type=str,
+        default='body.json'
+)
 
+# HTTPS request type
+parser.add_argument(
+        '--requestype',
+        nargs=1,
+        type=str,
+        default='post',
+        choices=['get', 'put', 'post']
+)
+
+# Generating json to send
+def generate_default_body(data):
+    return json.dumps({
+            "data": data
+    })
+
+functions = {
+    "testFunction": generate_default_body("recipe_id")
 }
 
-body = {
+args = parser.parse_args()
 
-}
+if args.local[0] == True:
+    based_url = "https:localhost:4000/functions/"
+else:
+    base_url = "https://somethingelseidontremember"
 
-# Send request
-print("Calling url:", url)
 
-
-# Print results of request
+# Continue writing this later
