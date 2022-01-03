a0e5fc7e693c26ea6e7e97742fe59ade12183bb6
Darvesh Gorhe <dgorhe123@gmail.com>
2022-01-03 14:47:30 -0800

    moved web app to firebase hosting dir and updated kitchenpantry.app

---
+++ firebase/public/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Kitchen Pantry!
+        </p>
+        <p>
+          The site is currently under construction but you can learn more about the project below
+        </p>
+        <a className="App-link" href="https://github.com/dgorhe/kitchenpantry" target="_blank" rel="noopener noreferrer">
+          Learn more about Kitchen Pantry
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
